eb4f67250ddfc3a3b64a32a213de222016b20e62
Aftar Ahmad Sami <aftarahmadsami@gmail.com>
2024-04-05 17:15:13 +0600

    added pdf-base64 endpoint

--- ocr_ai_api.py
+++ ocr_ai_api.py
@@ -1,12 +1,16 @@
+import os
+import base64
+import io
+import cv2
+import pytesseract
+import uuid
 from functools import wraps
 from flask_cors import CORS
 from flask import Flask, request, jsonify, abort
-import cv2
-import pytesseract
-from pdf2image import convert_from_path
-import os
-import uuid
+from pdf2image import convert_from_path, convert_from_bytes
 from dotenv import load_dotenv
+
+
 
 # Define the base directory and the path of the environment file
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
@@ -82,7 +86,23 @@ def pdf_to_img(pdf_file):
         abort(500, description="Error in converting PDF to image")
 
 
+def pdf_to_img_base64(pdf_file):
+    """
+    Convert a PDF file to a list of images.
 
+    Args:
+        pdf_file (io.BytesIO): The file-like object of the PDF file.
+
+    Returns:
+        list: A list of images converted from the PDF file.
+
+    Raises:
+        HTTPException: If there is an error in converting the PDF to image.
+    """
+    try:
+        return convert_from_bytes(pdf_file.read())
+    except Exception:
+        abort(500, description="Error in converting PDF to image")
 
 
 def ocr_core(file):
@@ -126,7 +146,25 @@ def ocr_pdf():
     return jsonify(response)
 
 
+@app.route('/pdf-base64', methods=['POST'])
+@require_api_key
+def ocr_pdf_base64():
+    """
+    Extracts text from a PDF file uploaded via POST request.
 
+    Returns:
+        dict: A JSON response containing the extracted text for each page of the PDF.
+    """
+    pdf_base64 = request.json.get('file')
+    if not pdf_base64:
+        abort(400, description="No file found in request")
+    pdf_bytes = base64.b64decode(pdf_base64)
+    pdf_file = io.BytesIO(pdf_bytes)
+    images = pdf_to_img_base64(pdf_file)
+    response = {pg+1: ocr_core(img) for pg, img in enumerate(images)}
+    return jsonify(response)
+    
+    
 
 @app.route('/image', methods=['POST'])
 @require_api_key
@@ -141,9 +179,15 @@ def ocr_image():
     if not image_file:
         abort(400, description="No file found in request")
     unique_filename = str(uuid.uuid4())
-    image_path = os.path.join(BASE_DIR, 'uploads', 'image', f'{unique_filename}.jpg')
+    image_path = os.path.join(BASE_DIR, 'uploads', 'image', f'{unique_filename}')
     image_file.save(image_path)
     img = cv2.imread(image_path)
     text = ocr_core(img)
     os.remove(image_path)
     return text
+    
+    
+
+
+
+
